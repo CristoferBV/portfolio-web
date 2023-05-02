633b88976ab48e03c681c16792250f4cc05e79ab
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-02 17:02:16 -0600

    Component Main
    Se creo el componente Main, que es la parte principal o de inicio (Home) por así decirlo.

--- personal-portfolio/pages/index.tsx
+++ personal-portfolio/pages/index.tsx
@@ -1,3 +1,4 @@
+import Main from '@/components/Main';
 import Navbar from '@/components/Navbar';
 import Head from 'next/head';
 
@@ -9,7 +10,8 @@ export default function Home() {
         <title>Cris | Front-End Portfolio</title>
 	      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css" integrity="sha512-+mK7mpu8cAOfre7c/51eKpX9/1jKfo1lw5+lMok5/vZb5K5Q5G1O28jKYZ+BClXoxOdi2cF6Ulu+6KoJp55lIQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
-    <Navbar/>  
+    <Navbar/>
+    <Main/>
 
     </div>
   )
