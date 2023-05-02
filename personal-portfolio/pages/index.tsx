b6c533019fcd5a217667b38092856593338deccf
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-02 15:34:03 -0600

    Component navBar
    Se creó el componente navBar (barra de navegación) tanto para la visualización en desktop y en mobile.

--- personal-portfolio/pages/index.tsx
+++ personal-portfolio/pages/index.tsx
@@ -1,20 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
 import Head from 'next/head';
-import SecctionAboutMe from '@/components/SecctionAboutMe';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Portfolio CrisDev</title>
+        <title>Cris | Front-End Portfolio</title>
 	      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css" integrity="sha512-+mK7mpu8cAOfre7c/51eKpX9/1jKfo1lw5+lMok5/vZb5K5Q5G1O28jKYZ+BClXoxOdi2cF6Ulu+6KoJp55lIQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
-      <Header/>
-      <SecctionAboutMe/>
+    <Navbar/>  
+
     </div>
-  );
-};
+  )
+}
