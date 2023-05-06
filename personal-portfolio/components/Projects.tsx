819450640eec457c8b771e4122df85a1ee27207b
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-06 11:41:48 -0600

    Update component project

--- personal-portfolio/components/Projects.tsx
+++ personal-portfolio/components/Projects.tsx
@@ -1,12 +1,32 @@
 import React from "react";
-import propertyImg from '../public/assets/projects/property.jpg'
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
-import ProjectItem from "./ProjectItem";
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 const Projects = () => {
+  const swiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    navigation: true,
+    pagination: true,
+  };
+
+  const handleImageError = (e: any) => {
+    console.error("Error cargando la imagen", e.target.src);
+  };
+
+  const swiperContainerStyles = {
+    maxWidth: "1100px",
+    height: "700px",
+    maxHeight: "calc(100vw - 40px)",
+  };
+
   return (
     <div className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
@@ -14,27 +34,34 @@ const Projects = () => {
           Projects
         </p>
         <h2 className="py-4">What I`ve Built</h2>
-        <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem
-            title="Property Finder"
-            backgroundImg={propertyImg}
-            projectUrl="/property"
-          />
-          <ProjectItem
-            title="Crypto App"
-            backgroundImg={cryptoImg}
-            projectUrl="/property"
-          />
-          <ProjectItem
-            title="Property Finder"
-            backgroundImg={netflixImg}
-            projectUrl="/property"
-          />
-          <ProjectItem
-            title="Property Finder"
-            backgroundImg={twitchImg}
-            projectUrl="/property"
-          />
+        <div className="m-auto py-12">
+        <div className="bg-gray-100 max-w-screen-lg mx-auto shadow-xl rounded-xl ">
+            <div className="shadow-lg rounded-md overflow-hidden">
+              <Swiper {...swiperOptions} style={swiperContainerStyles}>
+                <SwiperSlide>
+                  <img
+                    src="/assets/projects/crypto.jpg"
+                    alt="Crypto"
+                    onError={handleImageError}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="/assets/projects/netflix.jpg"
+                    alt="Netflix"
+                    onError={handleImageError}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="/assets/projects/property.jpg"
+                    alt="Property"
+                    onError={handleImageError}
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
         </div>
       </div>
     </div>
